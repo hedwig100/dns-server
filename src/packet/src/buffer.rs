af1ee445f8634391d2a196107bb7d6434538cab2
hedwig100 <pentagon7951@outlook.jp>
2023-02-18 23:38:15 +0900

    [add] packet package and add BytePacketBuffer

---
+++ src/packet/src/buffer.rs
@@ -0,0 +1,130 @@
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+pub struct BytePacketBuffer {
+    pub buf: [u8; 512],
+    pub pos: usize,
+}
+
+impl BytePacketBuffer {
+    pub fn new() -> BytePacketBuffer {
+        BytePacketBuffer {
+            buf: [0; 512],
+            pos: 0,
+        }
+    }
+
+    fn pos(&self) -> usize {
+        self.pos
+    }
+
+    pub fn step(&mut self, steps: usize) -> Result<()> {
+        self.pos += steps;
+        Ok(())
+    }
+
+    fn seek(&mut self, pos: usize) -> Result<()> {
+        self.pos = pos;
+        Ok(())
+    }
+
+    /// Read a single byte and move the position one step forward
+    fn read(&mut self) -> Result<u8> {
+        if self.pos >= 512 {
+            return Err("End of buffer".into());
+        }
+        let res = self.buf[self.pos];
+        self.pos += 1;
+
+        Ok(res)
+    }
+
+    /// Get a single byte, without changin the buffer position
+    fn get(&mut self, pos: usize) -> Result<u8> {
+        if pos >= 512 {
+            return Err("End of buffer".into());
+        }
+        Ok(self.buf[pos])
+    }
+
+    fn get_range(&mut self, start: usize, len: usize) -> Result<&[u8]> {
+        if start + len >= 512 {
+            return Err("End of buffer".into());
+        }
+        Ok(&self.buf[start..start + len as usize])
+    }
+
+    pub fn read_u16(&mut self) -> Result<u16> {
+        let res = ((self.read()? as u16) << 8) | (self.read()? as u16);
+        Ok(res)
+    }
+
+    pub fn read_u32(&mut self) -> Result<u32> {
+        let res = ((self.read()? as u32) << 24)
+            | ((self.read()? as u32) << 16)
+            | ((self.read()? as u32) << 8)
+            | self.read()? as u32;
+        Ok(res)
+    }
+
+    /// Read a qname
+    ///
+    /// The tricky part: Reading domain names, taking labels into consideration.
+    /// Will take something like [3]www[6]google[3]com[0] and append
+    /// www.google.com to outstr
+    pub fn read_qname(&mut self, outstr: &mut String) -> Result<()> {
+        let mut pos = self.pos();
+
+        let mut jumped = false;
+        let max_jumps = 5;
+        let mut jumps_performed = 0;
+
+        let mut delim = "";
+        loop {
+            if jumps_performed > max_jumps {
+                return Err(format!("Limit of {} jumps exceeded", max_jumps).into());
+            }
+
+            let len = self.get(pos)?;
+
+            // If len has the two most significant bit are set,
+            // it represents a jump to some other offset in the packet:
+            if (len & 0xC0) == 0xC0 {
+                if !jumped {
+                    self.seek(pos + 2)?;
+                }
+
+                let b2 = self.get(pos + 1)? as u16;
+                let offset = (((len as u16) ^ 0xC0) << 8) | b2;
+                pos = offset as usize;
+
+                jumped = true;
+                jumps_performed += 1;
+
+                continue;
+            }
+            // The base scenario, where we're reading a single label and
+            // appending it to the output
+            else {
+                pos += 1;
+
+                if len == 0 {
+                    break;
+                }
+
+                outstr.push_str(delim);
+                let str_buffer = self.get_range(pos, len as usize)?;
+                outstr.push_str(&String::from_utf8_lossy(str_buffer).to_lowercase());
+
+                delim = ".";
+                pos += len as usize;
+            }
+        }
+
+        if !jumped {
+            self.seek(pos)?;
+        }
+
+        Ok(())
+    }
+}
