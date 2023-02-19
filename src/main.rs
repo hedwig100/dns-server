e3d07bf69c32cd05cea0d3b26e7d14750d568252
hedwig100 <pentagon7951@outlook.jp>
2023-02-19 20:40:58 +0900

    [add] packet analysis code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use dns_server::error::Result;
+use packet::{buffer, DnsPacket};
+use std::fs::File;
+use std::io::Read;
+
+fn main() -> Result<()> {
+    let mut f = File::open("response_packet.txt")?;
+    let mut buffer = buffer::BytePacketBuffer::new();
+    f.read(&mut buffer.buf)?;
+
+    let packet = DnsPacket::from_buffer(&mut buffer)?;
+    println!("{:#?}", packet.header);
+
+    for q in packet.questions {
+        println!("{:#?}", q);
+    }
+    for rec in packet.answers {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.authorities {
+        println!("{:#?}", rec);
+    }
+    for rec in packet.resources {
+        println!("{:#?}", rec);
+    }
+
+    Ok(())
 }
