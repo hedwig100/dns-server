211a659e3652360a0ff27ba041448e19beb628a7
hedwig100 <shota.in26@gmail.com>
2023-03-12 22:26:47 +0900

    [add] recursive lookup

--- src/server/src/lib.rs
+++ src/server/src/lib.rs
@@ -5,11 +5,14 @@ use packet;
 use packet::buffer;
 use packet::header;
 use packet::query;
+use std::net;
 use std::net::UdpSocket;
 
-fn lookup(qname: &str, qtype: query::QueryType) -> Result<packet::DnsPacket> {
-    let server = ("8.8.8.8", 53);
-
+fn lookup(
+    qname: &str,
+    qtype: query::QueryType,
+    server: (net::Ipv4Addr, u16),
+) -> Result<packet::DnsPacket> {
     let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
     let mut packet = packet::DnsPacket::new();
@@ -32,6 +35,41 @@ fn lookup(qname: &str, qtype: query::QueryType) -> Result<packet::DnsPacket> {
     packet::DnsPacket::from_buffer(&mut res_buffer)
 }
 
+fn recursive_lookup(qname: &str, qtype: query::QueryType) -> Result<packet::DnsPacket> {
+    // For now we're alwasy starting with *a.root-servers.net*.
+    let mut ns = "198.41.0.4".parse::<net::Ipv4Addr>().unwrap();
+
+    loop {
+        println!("attempting lookup of {:?} {} with ns {}", qtype, qname, ns);
+
+        let ns_copy = ns;
+        let server = (ns_copy, 53);
+        let response = lookup(qname, qtype, server)?;
+
+        if !response.answers.is_empty() && response.header.rescode == header::ResultCode::NOERROR {
+            return Ok(response);
+        }
+
+        if let Some(new_ns) = response.get_resolved_ns(qname) {
+            ns = new_ns;
+            continue;
+        }
+
+        let new_ns_name = match response.get_unresolved_ns(qname) {
+            Some(x) => x,
+            None => return Ok(response),
+        };
+
+        let recursive_response = recursive_lookup(&new_ns_name, query::QueryType::A)?;
+
+        if let Some(new_ns) = recursive_response.get_random_a() {
+            ns = new_ns;
+        } else {
+            return Ok(response);
+        }
+    }
+}
+
 pub fn handle_query(socket: &UdpSocket) -> Result<()> {
     let mut req_buffer = buffer::BytePacketBuffer::new();
 
@@ -48,7 +86,7 @@ pub fn handle_query(socket: &UdpSocket) -> Result<()> {
     if let Some(question) = request.questions.pop() {
         println!("Received query: {:?}", question);
 
-        if let Ok(result) = lookup(&question.name, question.qtype) {
+        if let Ok(result) = recursive_lookup(&question.name, question.qtype) {
             pkt.questions.push(question);
             pkt.header.rescode = result.header.rescode;
 
